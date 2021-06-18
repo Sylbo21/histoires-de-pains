596e98034dc39a7e24a3f1c4f8cdab20d1849e70
sylviane <sylviane.borel.21@gmail.com>
2021-06-18 17:53:14 +0200

    Add one marker to map

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,10 +2,16 @@
 // All this logic will automatically be available in application.js.
 
 function initMap(lat, lng) {
-    var myCoords = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
+  var myCoords = new google.maps.LatLng(lat, lng);
+  var mapOptions = {
     center: myCoords,
     zoom: 14
-    };
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCoords,
+    map: map
+});
 }
