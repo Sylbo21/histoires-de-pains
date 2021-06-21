a72f3de1a289cca867bb761df3763e867fbc30a6
sylviane <sylviane.borel.21@gmail.com>
2021-06-21 11:44:25 +0200

    Update seed data with Rounds and Locations-Rounds associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@ location2 = Location.create! name: 'Palézieux-Village',
 
 location3 = Location.create! name: 'Corcelles-sur-Chavornay',
                              address: 'Le Château 3, 1374 Corcelles-sur-Chavornay'
+
+round1 = Round.create! day: 'Lundi'
+round2 = Round.create! day: 'Mardi'
+round3 = Round.create! day: 'Mercredi'
+
+round1.locations << location1
+round1.locations << location2
+round2.locations << location3
+round3.locations << location1
+round3.locations << location3
