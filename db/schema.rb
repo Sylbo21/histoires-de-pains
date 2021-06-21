2a29dbc23708c16c25ebecf9820b1b6b33215ed7
sylviane <sylviane.borel.21@gmail.com>
2021-06-21 11:17:44 +0200

    Create Round model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210617045525) do
+ActiveRecord::Schema.define(version: 20210621091631) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20210617045525) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "rounds", force: :cascade do |t|
+    t.string "day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
