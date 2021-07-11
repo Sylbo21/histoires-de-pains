30a134711b3bff2ca8753dab650eb56fa59680a7
sylviane <sylviane.borel.21@gmail.com>
2021-07-11 17:57:23 +0200

    Add validation tests for location model

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -77,4 +77,32 @@ class LocationTest < ActiveSupport::TestCase
     assert_equal(location.rounds.first.day, "Mardi")
   end
 
+  test 'location has no name' do
+    location = Location.new address: '10 Downing Street, London'
+    location.save!
+    refute location.valid?
+  end
+
+  test 'location has no address' do
+    location = Location.new name: 'St-Petersburg'
+    location.save!
+    refute location.valid?
+  end
+
+  test 'latitude gets created after validation' do
+    # Gives an error (normal?) but test passes
+    location = Location.new name: 'London',
+                            address: '10 Downing Street, London'
+    location.save!
+    refute_equal(location.latitude, nil)
+  end
+
+  test 'longitude gets created after validation' do
+    # Gives an error (normal?) but test passes 
+    location = Location.new name: 'Paris',
+                            address: 'Av. des Champs-Elysées 2, Paris'
+    location.save!
+    refute_equal(location.longitude, nil)
+  end
+
 end
