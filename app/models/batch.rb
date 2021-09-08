05c9a37075cdafa69d5fc674c0d11a282a4a1afc
sylviane <sylviane.borel.21@gmail.com>
2021-09-08 16:07:38 +0200

    Include today in future batches

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -4,7 +4,7 @@ class Batch < ApplicationRecord
   validates :date, presence: true
   validates :date, uniqueness: true
 
-  scope :future,         -> { where('date > ?', Date.today) }
+  scope :future,         -> { where('date >= ?', Date.today) }
   scope :chronological,  -> { order(date: :asc) }
   scope :next_ones,      -> { future.chronological }
   scope :very_next_ones, -> { next_ones.limit(3) }
