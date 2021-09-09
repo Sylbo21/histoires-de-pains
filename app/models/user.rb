a5947d61f888e3e3d7b306580db77b9e76fb354a
sylviane <sylviane.borel.21@gmail.com>
2021-09-09 10:36:05 +0200

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :email, uniqueness: true
+  validates :role, inclusion: { in: %w(registered admin) }
+
+  after_initialize :default_role!
+  before_validation :downcase_email
+
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def default_role!
+    self.role ||= 'registered'
+  end
+
 end
