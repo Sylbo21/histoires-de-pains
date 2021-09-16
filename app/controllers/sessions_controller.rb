a58a8a3378f272efe2862799c3b7c0607f64bde3
sylviane <sylviane.borel.21@gmail.com>
2021-09-16 10:57:04 +0200

    Complete sessions#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if(user.present? && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      # change redirect_to 'My Account'
+      redirect_to root_path
+    else
+      flash[:alert] = "Email or password is invalid. Please try again."
+      render 'new'
+    end
   end
 
   def destroy
