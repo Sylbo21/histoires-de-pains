6d3e709fa15150f2af7a9b82c3ad3916afb1750c
sylviane <sylviane.borel.21@gmail.com>
2021-09-16 10:25:49 +0200

    Add sessions ressource

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
     root to: "locations#index"
   end
 
-  get 'home/index'
-
   root to: 'home#index'
 
   get 'home/index'
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :locations
 
