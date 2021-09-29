aa6084867d9b9c13427c7d3a028a3c7a3eca23bb
sylviane <sylviane.borel.21@gmail.com>
2021-09-29 17:46:00 +0200

    session#create and user#create: redirect to account_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@ class SessionsController < ApplicationController
 
     if(user.present? && user.authenticate(params[:password]))
       session[:user_id] = user.id
-      # change redirect_to 'My Account'
-      redirect_to root_path
+      redirect_to account_path
     else
       flash[:alert] = "L'email et/ou le mot de passe ne sont pas valides. Veuillez réessayer."
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if(@user.save)
       session[:user_id] = @user.id
-      # change redirect_to 'My Account'
-      redirect_to root_path
+      redirect_to account_path
     else
       render 'new'
     end
