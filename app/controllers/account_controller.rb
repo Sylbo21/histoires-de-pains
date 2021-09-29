ee4026dc7899d9a178634b46f2922fbf372c27dc
sylviane <sylviane.borel.21@gmail.com>
2021-09-29 15:56:12 +0200

    current_user helper method

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,6 +2,8 @@ class AccountController < ApplicationController
 
   before_action :ensure_authenticated
 
+  helper_method :current_user
+
   def show
   end
 
@@ -14,4 +16,9 @@ class AccountController < ApplicationController
   def ensure_authenticated
     redirect_to login_path unless(logged_in?)
   end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
 end
