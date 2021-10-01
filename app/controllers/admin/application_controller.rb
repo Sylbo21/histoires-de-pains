82136be2b219459ee297f8bfd9c99e9ba7dce243
sylviane <sylviane.borel.21@gmail.com>
2021-10-01 15:01:47 +0200

    Secure admin section

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -6,10 +6,26 @@
 # you're free to overwrite the RESTful controller actions.
 module Admin
   class ApplicationController < Administrate::ApplicationController
+    before_action :ensure_authenticated
     before_action :authenticate_admin
+
+    # Why aren't logged_in?, ensure_authenticated and current_user methods available here as they are in main ApplicationController? Why do I have to repeat them here?
+
+    def logged_in?
+      session[:user_id].present?
+    end
+
+    def ensure_authenticated
+      redirect_to login_path unless(logged_in?)
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
 
     def authenticate_admin
       # TODO Add authentication logic here.
+      redirect_to(login_path) unless current_user.role == 'admin'
     end
 
     # Override this value to specify the number of elements to display at a time
