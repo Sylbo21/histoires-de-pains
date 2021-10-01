694cc6920bf19f6913dae3519ff2db91d3c83403
sylviane <sylviane.borel.21@gmail.com>
2021-10-01 15:18:45 +0200

    Implement admin nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?, :current_user
+  helper_method :logged_in?, :current_user, :is_admin?
 
   def logged_in?
     session[:user_id].present?
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     User.find(session[:user_id])
+  end
+
+  def is_admin?
+    logged_in? && current_user.role == 'admin'
   end
 
 end
